OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Differentiate roaming disabled by supplicant and on iface
Currently, do_not_roam flag is used for denoting both supplicant
disabled roaming and the roaming disabled on an interface during
the interface bring up. Differentiate these two flags with proper
nomenclature to make it clear and distinguishable. Name the new
flags to be supplicant_disabled_roaming and roaming_allowed_on_iface.

Currently in a sta+sta scenario roaming is supported only on wlan0.
Based on the flag roaming_allowed_on_iface, send/discard the
roaming commands to be sent to the firmware.

Change-Id: I1bd19e938b00c26c06ff06c73754560d4502ef2c
CRs-Fixed: 2130204

core/hdd/src/wlan_hdd_cfg80211.c
@@ -15014,7 +15014,11 @@ static int wlan_hdd_cfg80211_connect_start(hdd_adapter_t *pAdapter,
 		qdf_mem_copy((void *)(pRoamProfile->SSIDs.SSIDList->SSID.ssId),
 			     ssid, ssid_len);
 
-		pRoamProfile->do_not_roam = !pAdapter->fast_roaming_allowed;
+		pRoamProfile->supplicant_disabled_roaming =
+			!pAdapter->fast_roaming_allowed;
+		pRoamProfile->roaming_allowed_on_iface =
+			pAdapter->fast_roaming_allowed;
+
 		/* cleanup bssid hint */
 		qdf_mem_zero(pRoamProfile->bssid_hint.bytes,
 			QDF_MAC_ADDR_SIZE);
@@ -15023,7 +15027,7 @@ static int wlan_hdd_cfg80211_connect_start(hdd_adapter_t *pAdapter,
 
 		if (bssid) {
 			pRoamProfile->BSSIDs.numOfBSSIDs = 1;
-			pRoamProfile->do_not_roam = true;
+			pRoamProfile->supplicant_disabled_roaming = true;
 			qdf_mem_copy((void *)(pRoamProfile->BSSIDs.bssid),
 				     bssid, QDF_MAC_ADDR_SIZE);
 			/*

PixelExperience-Devices/kernel_xiaomi_sm8350

core/hdd/src/wlan_hdd_cfg80211.c
@@ -15641,7 +15641,11 @@ static int wlan_hdd_cfg80211_connect_start(struct hdd_adapter *adapter,
 		qdf_mem_copy((void *)(roam_profile->SSIDs.SSIDList->SSID.ssId),
 			     ssid, ssid_len);
 
-		roam_profile->do_not_roam = !adapter->fast_roaming_allowed;
+		roam_profile->supplicant_disabled_roaming =
+			!adapter->fast_roaming_allowed;
+		roam_profile->roaming_allowed_on_iface =
+			adapter->fast_roaming_allowed;
+
 		/* cleanup bssid hint */
 		qdf_mem_zero(roam_profile->bssid_hint.bytes,
 			QDF_MAC_ADDR_SIZE);
@@ -15650,7 +15654,7 @@ static int wlan_hdd_cfg80211_connect_start(struct hdd_adapter *adapter,
 
 		if (bssid) {
 			roam_profile->BSSIDs.numOfBSSIDs = 1;
-			roam_profile->do_not_roam = true;
+			roam_profile->supplicant_disabled_roaming = true;
 			qdf_mem_copy((void *)(roam_profile->BSSIDs.bssid),
 				     bssid, QDF_MAC_ADDR_SIZE);
 			/*