OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Add ini param to control RTT mac randomization
Add change to allow randomizing mac address used in STA mode RTT
ranging using ini param control.

Change-Id: Ief3814ef758476d2617d8176daade2128c2b250a
CRs-Fixed: 2205953

core/hdd/src/wlan_hdd_main.c
@@ -3293,6 +3293,17 @@ QDF_STATUS hdd_init_station_mode(hdd_adapter_t *adapter)
 		}
 	}
 
+	if (adapter->device_mode == QDF_STA_MODE) {
+		hdd_debug("setting RTT mac randomization param: %d",
+			hdd_ctx->config->enable_rtt_mac_randomization);
+		ret_val = sme_cli_set_command(adapter->sessionId,
+			WMI_VDEV_PARAM_ENABLE_DISABLE_RTT_INITIATOR_RANDOM_MAC,
+			hdd_ctx->config->enable_rtt_mac_randomization,
+			VDEV_CMD);
+		if (0 != ret_val)
+			hdd_err("RTT mac randomization param set failed %d",
+				ret_val);
+	}
 	/*
 	 * 1) When DBS hwmode is disabled from INI then send HT/VHT IE as per
 	 *    non-dbs hw mode, so that there is no limitation applied for 2G/5G.

PixelExperience-Devices/kernel_xiaomi_sm8350

core/hdd/src/wlan_hdd_main.c
@@ -3905,6 +3905,18 @@ int hdd_vdev_create(struct hdd_adapter *adapter,
 		goto hdd_vdev_destroy_procedure;
 	}
 
+	if (adapter->device_mode == QDF_STA_MODE) {
+		hdd_debug("setting RTT mac randomization param: %d",
+			hdd_ctx->config->enable_rtt_mac_randomization);
+		errno = sme_cli_set_command(adapter->session_id,
+			WMI_VDEV_PARAM_ENABLE_DISABLE_RTT_INITIATOR_RANDOM_MAC,
+			hdd_ctx->config->enable_rtt_mac_randomization,
+			VDEV_CMD);
+		if (0 != errno)
+			hdd_err("RTT mac randomization param set failed %d",
+				errno);
+	}
+
 	hdd_info("vdev %d created successfully", adapter->session_id);
 
 	return 0;